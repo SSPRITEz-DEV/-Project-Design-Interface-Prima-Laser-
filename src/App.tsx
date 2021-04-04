86a09a7841942a2d888e7ae66d5d5fefe3b50284
unknown <tanawatt2541@gmail.com>
2021-04-04 18:05:23 +0700

    update: Added Sales page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import Navbar from './components/common/navbar/Navbar';
 import Landing from './components/landingPage/Landing';
 import Login from './components/loginPage/Login';
 import Registration from './components/registration/Registration';
+import Sales from './components/sales/Sales';
 
 function App() {
   return (
     <div className="App">
         {/* <Landing/> */}
-        <Login/>
+        {/* <Login/> */}
+        <Sales/>
+
         {/* <Registration/> */}
     </div>
   );
